5584c81fa001aea95c904461c902b8b21783e079
maria-kristine <m-kpedersen@hotmail.com>
2022-05-27 19:54:30 +0200

    add changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <div>
+        <Outlet />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './component/Home';
+import AllFlags from './component/AllFlags';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/> }>
+        <Route path="/home" element={<Home/> }/>
+        <Route path="/flag" element={<AllFlags /> }/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
